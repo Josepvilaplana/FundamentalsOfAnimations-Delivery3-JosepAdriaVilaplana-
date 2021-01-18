78a3a35670ab3cff5c3be4ecc238ce1ef769ddfc
Josepvilaplana <josepadriavilaplanamiret@enti.cat>
2021-01-18 21:28:20 +0100

    Exercise 1.2 working

--- Delivery3/Assets/MovingBall.cs
+++ Delivery3/Assets/MovingBall.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MovingBall : MonoBehaviour
 {
     [SerializeField]
     IK_tentacles _myOctopus;
+    [SerializeField]
+    GameObject _target;
 
     //movement speed in units per second
     [Range(-1.0f, 1.0f)]
@@ -14,15 +17,21 @@ public class MovingBall : MonoBehaviour
 
     Vector3 _dir;
 
+    public LineRenderer lineVisual;
+    public int lineSegment = 10;
+    Vector3 vo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lineVisual.positionCount = lineSegment;
     }
 
     // Update is called once per frame
     void Update()
     {
+        vo = (_target.transform.position - transform.position).normalized * 100;
+
         transform.rotation = Quaternion.identity;
 
         //get the Input from Horizontal axis
@@ -31,12 +40,34 @@ public class MovingBall : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         //update the position
-        transform.position = transform.position + new Vector3(-horizontalInput * _movementSpeed * Time.deltaTime, verticalInput * _movementSpeed * Time.deltaTime, 0);
-
+        //transform.position = transform.position + new Vector3(-horizontalInput * _movementSpeed * Time.deltaTime, verticalInput * _movementSpeed * Time.deltaTime, 0);
+        VisualizeLine(vo);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         _myOctopus.NotifyShoot();
+    }
+
+    private Vector3 CalculatePosInTime(Vector3 vo, float time)
+    {
+        Vector3 vxz = vo;
+        vxz.y = 0f;
+
+        Vector3 result = transform.position + vo * time;
+        float sY = (-0.5f * Mathf.Abs(Physics.gravity.y) * (time * time)) + (vo.y * time) + transform.position.y;
+
+        result.y = sY;
+
+        return result;
+    }
+
+    void VisualizeLine(Vector3 vo)
+    {
+        for (int i = 0; i < lineSegment; i++)
+        {
+            Vector3 pos = CalculatePosInTime(vo, i / (float)lineSegment);
+            lineVisual.SetPosition(i, pos);
+        }
     }
 }
